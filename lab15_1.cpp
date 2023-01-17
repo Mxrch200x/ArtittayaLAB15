2b22a0970312e63c9194afee446ddc035076fc2d
Mxrch200x <gaerdbigsis@gmail.com>
2023-01-17 16:20:07 +0700

    cpmpleted

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,27 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	T k, l;
+	int j;
+	for(int i = 1; i < N; i++) {
+		k = d[i];
+		j = i - 1;
+		while (j >= 0 && d[j] < k){
+			l = k;
+			d[j+1] = d[j];
+			d[j] = l;
+			j--;
+		}
+		cout << "Pass " << i << ":";
+		for(int l = 0; l < N; l++){
+			cout << d[l] << " ";
+		}
+	cout << endl;
+	}	
 }
+	
+			
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
